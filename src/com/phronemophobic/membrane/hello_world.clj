5e4dd860306a50b1219403dc14d2ff5ecd254d28
Omer Zak <w1@zak.co.il>
2024-03-17 09:48:48 +0200

    Add println and an attempt to set java.library.path property. Did not help fix the problem of abort due to failure to "allocate library name".

--- src/com/phronemophobic/membrane/hello_world.clj
+++ src/com/phronemophobic/membrane/hello_world.clj
@@ -6,14 +6,12 @@
   (:gen-class))
 
 (defn -main [& _args]
+  (println "Trying to print something")
+  (println "java.library.path =" (java.lang.System/getProperty "java.library.path"))
+  (java.lang.System/setProperty "java.library.path" (str "/home/omer/work/Projects/Clojure/BabashkaPodWork/membrane-native-image:" (java.lang.System/getProperty "java.library.path")))
+  (println "java.library.path =" (java.lang.System/getProperty "java.library.path"))
+  (java2d/run
+    (membrane.component/make-app
+     #'td/todo-app
+     td/todo-state)))
 
-  (println
-   (str
-    (ui/bounds
-     ((membrane.component/make-app
-       #'td/todo-app
-       td/todo-state)))))
-  #_(java2d/run
-      (membrane.component/make-app
-       #'td/todo-app
-       td/todo-state)))
