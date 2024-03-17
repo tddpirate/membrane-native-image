9e1b3ba73912e7308e398e74228bc858224868b4
Omer Zak <w1@zak.co.il>
2024-03-17 23:03:16 +0200

    My own config.sh modifications before actual rebase.

--- config.sh
+++ config.sh
@@ -3,6 +3,22 @@
 set -e
 set -x
 
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "Please set GRAALVM_HOME"
+    exit 1
+fi
+
+if [ ! -e "${GRAALVM_HOME}/bin/native-image" ]; then
+    echo "Your ${GRAALVM_HOME} does not have native-image, aborting."
+    exit 1
+fi
+
+export JAVA_HOME=$GRAALVM_HOME
+export PATH=$GRAALVM_HOME/bin:$PATH
+
+which java
+java -version
+
 clojure -T:build compile
 
 # not currently needed
