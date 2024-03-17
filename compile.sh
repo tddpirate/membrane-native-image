faf949ce121497b7e23f1985be915b89d8339efe
Omer Zak <w1@zak.co.il>
2024-03-17 08:25:13 +0200

    Add some *_HOME setting code to compile.sh Also the H:+UnlockExperimentalVMOptions flag (to eliminate a warning message).

--- compile.sh
+++ compile.sh
@@ -3,6 +3,19 @@
 set -e
 set -x
 
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "Please set GRAALVM_HOME"
+    exit 1
+fi
+
+if [ ! -e "${GRAALVM_HOME}/bin/native-image" ]; then
+    echo "Your ${GRAALVM_HOME} does not have native-image, aborting."
+    exit 1
+fi
+
+export JAVA_HOME=$GRAALVM_HOME
+export PATH=$GRAALVM_HOME/bin:$PATH
+
 which java
 java -version
 
@@ -13,6 +26,7 @@ clojure -T:build:native-image compile
 
 native-image \
     -cp "$(clojure -Spath):target/classes" \
+    -H:+UnlockExperimentalVMOptions \
     -H:Name=hello-world \
     -Djava.awt.headless=false \
     -H:+ReportExceptionStackTraces \
